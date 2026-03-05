363900180b4b2d0437dc5b588bf04378429429d5
Karen Handy <karen.handy@cmcss.net>
2026-03-04 18:32:45 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,5 +2,6 @@ torch_lit = True
 if torch_lit :
     outcome = "Flicker: Use to light up hallway."
 else:
-    outcome = "Doom: Look for another torch."
+    outcome = "Doom: Look for another torch. "
 print(outcome)
+
